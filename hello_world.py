559e769177d803dab3a7b5614c3d6e7c57558661
Hamza Irshad <86020938+hamzairshad-cs@users.noreply.github.com>
2024-09-07 18:07:54 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,10 @@
 print("Hello world!")
-<<<<<<< HEAD
+
 
 print("Hello once again")
 
 print("Hello world!")
 print("Hello darkness my old friend")
 
-=======
+
 print("I've come to talk with you again")
->>>>>>> new-remote/feature/modify-print

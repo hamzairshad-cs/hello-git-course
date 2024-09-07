87efcd038bfb41278ee3303fd3fd0f7ff74ab5aa
Hamza Irshad <86020938+hamzairshad-cs@users.noreply.github.com>
2024-09-07 18:03:31 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-
 print("Hello world!")
 
 print("Hello once again")
-<<<<<<< HEAD
->>>>>>> intermediate-normal-merge/feature/add-logging
-=======
 
->>>>>>> 97dc1fe0460aef0b01bb40e5081ea26838430e0f
-=======
 print("Hello world!")
 print("Hello darkness my old friend")
->>>>>>> new-remote/master
-=======
->>>>>>> origin/main
+

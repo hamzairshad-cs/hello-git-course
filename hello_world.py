97dc1fe0460aef0b01bb40e5081ea26838430e0f
Hamza Irshad <86020938+hamzairshad-cs@users.noreply.github.com>
2024-09-07 17:05:31 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 print("Hello World!");
-=======
+
 print("Hello world!")
->>>>>>> intermediate/master
-=======
+
 print("Hello World!")
->>>>>>> origin/main
-=======
+
 print("Hello world!")
->>>>>>> intermediate-normal-merge/master
-=======
+
 print("Hello world!");
 
 
->>>>>>> origin/main
+

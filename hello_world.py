3222615ef5326f5aca9dc7a10a9bbe612d2cd48d
Hamza Irshad <86020938+hamzairshad-cs@users.noreply.github.com>
2024-09-07 17:14:56 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,27 +1,10 @@
 
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> intermediate/master
-=======
-=======
 
-<<<<<<< HEAD
->>>>>>> 97dc1fe0460aef0b01bb40e5081ea26838430e0f
 print("Hello World!")
 
 print("Hello world!")
 
 print("Hello world!");
-=======
->>>>>>> origin/main
 
-
-<<<<<<< HEAD
->>>>>>> origin/main
-=======
 print("Hello once again")
->>>>>>> intermediate-normal-merge/feature/add-logging
-=======
-
->>>>>>> 97dc1fe0460aef0b01bb40e5081ea26838430e0f

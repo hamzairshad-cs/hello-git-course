93433bc1a597df994bd8356bf86678a29c682a44
Hamza Irshad <86020938+hamzairshad-cs@users.noreply.github.com>
2024-09-07 16:52:26 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 print("Hello World!");
-=======
+
 print("Hello world!")
->>>>>>> intermediate/master
-=======
+
 print("Hello World!")
->>>>>>> origin/main
+
 print("Hello from feature")
 
 print("Hello from release")
